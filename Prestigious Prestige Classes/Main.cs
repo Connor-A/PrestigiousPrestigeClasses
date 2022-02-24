be1963cf8c2b36053c01d465fee992da77577abb
Conno <Conno@DESKTOP-LO3NOUR>
2022-02-24 08:08:06 -0500

    Add project files.

---
+++ Prestigious Prestige Classes/Main.cs
@@ -0,0 +1,25 @@
+﻿using UnityModManagerNet;
+using HarmonyLib;
+using System.Reflection;
+
+namespace Prestigious_Prestige_Classes
+{
+    static class Main
+    {
+        public static bool Enabled;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            modEntry.OnToggle = OnToggle;
+            var harmony = new Harmony(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            return true;
+        }
+
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            Enabled = value;
+            return true;
+        }
+    }
+}
---
+++ Prestigious Prestige Classes/Reworks/01_PPC_EldritchKnight.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prestigious_Prestige_Classes.Reworks
+{
+    internal class EldritchKnightRework
+    {
+    }
+}
